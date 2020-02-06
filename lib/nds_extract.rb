9b63740c6a74a6617bb2da2df8c426f34f153ae2
Shervon Ceromain <shervonc@gmail.com>
2020-02-06 13:38:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,7 +9,7 @@ def directors_totals(nds)
   while d_index < nds.length do
   m_index = 0
    d_name = nds[:name]
-     m_len = nds[:name][:movies].length
+     m_len = 
      while m_index < m_len
      result << dname  total += nds[:name][:movies][:worldwide_gross]
     m_index += 1
